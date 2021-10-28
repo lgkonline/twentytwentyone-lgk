891f810cd38c45159be229fc523a052b428c875c
Lars Kliesing <lgk@live.de>
2021-10-28 11:30:01 +0200

    Added PUC

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/lgkonline/twentytwentyone-lgk',
+	__FILE__,
+	'twentytwentyone-lgk'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
+
 /* CSS und Skripte vom Parent-Theme übernehmen */
    
 function twentytwentyone_styles() {

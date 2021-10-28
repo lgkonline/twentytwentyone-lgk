0c9b44949de66ef51886c17278130f9b430e41d9
Lars Kliesing <lgk@live.de>
2021-10-28 13:58:54 +0200

    Adadapted LGK.io styles

--- functions.php
+++ functions.php
@@ -18,6 +18,9 @@ $myUpdateChecker->setAuthentication('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');
 function twentytwentyone_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(),
 	array( 'twenty-twenty-one-style' ), wp_get_theme()->get('Version') );
+
+    wp_enqueue_style('lgk-icons', 'https://lib.lgkonline.com/lgk-icons/style.min.css?version=1.4.12');
+    wp_enqueue_style('hk-grotesk-font', 'https://lib.lgkonline.com/webfont/stylesheet.min.css');
 }
 
 add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
